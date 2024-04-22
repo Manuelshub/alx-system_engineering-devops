cbde78adeab95a87bb4c5d34708db46d7c95a1fc
Emmanuel <kcemmy03@gmail.com>
2024-04-23 00:31:06 +0100

    Modified the manifest

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,7 +6,7 @@ include stdlib
 file_line { 'ssh_private_key':
   path               => '/etc/ssh/ssh_config',
   line               => '    IdentityFile ~/.ssh/school',
-  match              => '^[#]+[\s]*(?i)IdentityFile\s+~\/\.ssh\/id_rsa$'
+  match              => '^[#]+[\s]*(?i)IdentityFile\s+~\/\.ssh\/id_rsa$',
   replace            => true,
   append_on_no_match => true
 }
@@ -14,7 +14,7 @@ file_line { 'ssh_private_key':
 file_line { 'password_auth':
   path               => '/etc/ssh/ssh_config',
   line               => '    PasswordAuthentication no',
-  match              => '^[#]+[\s]*(?i)PasswordAuthentication\s+(yes|no)$'
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication\s+(yes|no)$',
   replace            => true,
   append_on_no_match => true
 }

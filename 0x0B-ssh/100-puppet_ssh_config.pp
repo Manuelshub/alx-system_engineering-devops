0f59c573f7f619cdc215dd5e85c0bf8967e1f3b6
Emmanuel <kcemmy03@gmail.com>
2024-04-22 23:43:39 +0100

    Puppet manifest that edits a config file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,20 @@
+# This puppet file makes changes to our configuration file
+# so that one can connect to the server without password.
+
+include stdlib
+
+file_line { 'ssh_private_key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile\s+~\/\.ssh\/id_rsa$'
+  replace            => true,
+  append_on_no_match => true
+}
+
+file_line { 'password_auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication\s+(yes|no)$'
+  replace            => true,
+  append_on_no_match => true
+}

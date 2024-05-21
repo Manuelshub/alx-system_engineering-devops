e81c5ac020ddadee978ec54a984b17a6e2dda942
Emmanuel <kcemmy03@gmail.com>
2024-05-21 03:43:47 +0100

    Script that exports data extracted from API to csv file

---
+++ 0x15-api/1-export_to_csv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This Script fetches data from an API and exports to a csv file
+"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    employeeID = sys.argv[1]
+    url = f"https://jsonplaceholder.typicode.com/users/{employeeID}"
+    todos = f"https://jsonplaceholder.typicode.com/users/{employeeID}/todos"
+    user = requests.get(url).json()
+    username = user.get("name")
+    tasks = requests.get(todos).json()
+
+    with open(f"{employeeID}.csv", 'w') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for todo in tasks:
+            writer.writerow(
+                [employeeID,
+                 username, todo.get("completed"),
+                 todo.get("title")])

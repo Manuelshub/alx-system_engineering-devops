4003d4c28e3f768b6a7b02ce0995c8f2d6d7c7ca
Emmanuel <kcemmy03@gmail.com>
2024-05-21 03:19:54 +0100

    Script that fetches data about an employee

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This Script gathers data from an API.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    employeeID = sys.argv[1]
+    url = f"https://jsonplaceholder.typicode.com/users/{employeeID}"
+    todos = f"https://jsonplaceholder.typicode.com/users/{employeeID}/todos"
+    user = requests.get(url).json()
+    username = user.get("name")
+    tasks = requests.get(todos).json()
+    completed = [todo.get("title") for todo in tasks if todo.get("completed")]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        username, len(completed), len(tasks)))
+    
+    for task in completed:
+        print(f"\t {task}")

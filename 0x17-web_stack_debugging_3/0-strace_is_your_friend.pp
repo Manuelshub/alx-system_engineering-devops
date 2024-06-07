3f666b97bb97b54591d8f7447e4d120f9300f10a
Emmanuel <kcemmy03@gmail.com>
2024-06-07 09:51:14 +0100

    Chnaged file name

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,10 @@
 # A puppet manifest to replace a line in a server
 
-$filename = '/var/www/html/wp-settings.php'
+$file_to_edit = '/var/www/html/wp-settings.php'
 
 #replace line containing phpp to php
 
 exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${filename}",
-  path    => ['bin','usr/bin']
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }

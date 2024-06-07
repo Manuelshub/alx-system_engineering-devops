3d762a3992d99a987d17ddc7247673894f7b0674
Emmanuel <kcemmy03@gmail.com>
2024-06-07 04:05:48 +0100

    Puppet manifest

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+# A puppet manifest to replace a line in a server
+
+$filename = '/var/www/html/wp-settings.php'
+
+#replace line containing phpp to php
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${filename}",
+  path    => ['bin', 'usr/bin']
+}
